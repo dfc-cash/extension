44c0e24e182ceaa75b2c2c9556e874c74998f845
simpleelegant <simpleelegant@163.com>
2022-04-30 19:26:30 +0800

    implement

---
+++ packages/extension-base/src/index.ts
@@ -0,0 +1,6 @@
+// Copyright 2019-2021 @polkadot/extension-base authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import './detectPackage';
+
+export { packageInfo } from './packageInfo';
